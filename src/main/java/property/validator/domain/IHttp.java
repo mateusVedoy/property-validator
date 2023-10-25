2838ba2d149cb312174d8745bca2dc1e46e332da
mateusVedoy <mateusvedoy@outlook.com>
2023-10-25 11:01:29 -0300

    add final endpoint and cors

--- src/main/java/property/validator/domain/IHttp.java
+++ src/main/java/property/validator/domain/IHttp.java
@@ -11,7 +11,7 @@ import jakarta.ws.rs.core.MediaType;
 @RegisterRestClient()
 public interface IHttp {
     @POST
-    @Path("/7406f4b7-7a33-44e0-9864-708d9a79f3c4/{propertyId}/{isValid}")
+    @Path("/property/validation/{propertyId}/{isValid}")
     @Produces(MediaType.APPLICATION_JSON)
     void post(@PathParam("propertyId") Long propertyId, @PathParam("isValid") String isValid);
 }
