14a783ac08a39f25ea286286a295efab927e9134
mateusVedoy <mateusvedoy@outlook.com>
2023-10-25 11:06:02 -0300

    add schedule delete after notify

--- src/main/java/property/validator/application/useCase/NotifyRecipient.java
+++ src/main/java/property/validator/application/useCase/NotifyRecipient.java
@@ -26,6 +26,7 @@ public class NotifyRecipient {
         schedules.forEach(schedule -> {
             System.out.println("Prop id: " + schedule.getPropertyId() + " has been notified");
             http.post(schedule.getPropertyId(), schedule.getIsValid());
+            repository.removeByPropertyId(schedule.getPropertyId());
         });            
     }
 }
